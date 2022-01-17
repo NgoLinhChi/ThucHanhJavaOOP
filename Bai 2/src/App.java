f82d21103c8d101f599914c961dffb657e36ba29
Chi Linh <you@example.com>
2022-01-17 16:50:56 +0700

    Bai 2

---
+++ Bai 2/src/App.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class App {
+    public static void main(String[] args) throws Exception {
+        Scanner abc = new Scanner(System.in);
+        String strHovaTen = "";
+        System.out.println("Nhap ho ten: ");
+        strHovaTen = abc.nextLine();
+        System.out.println("Xin chao" + strHovaTen);
+    }
+}
